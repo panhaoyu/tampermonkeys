528b059222ec99bfafbf614b67920c76c6a5f0fd
wolfpan <haoyupan@aliyun.com>
2019-08-09 19:36:54 +0800

    update science direct prettify

--- scienceDirectPrettify.js
+++ scienceDirectPrettify.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Science Direct Prettify
 // @namespace    http://tampermonkey.net/
-// @version      0.3.1
+// @version      0.3.2
 // @description  Get a better performance when reading papers from science direct website.
 // @author       Haoyu Pan
 // @match        https://www.sciencedirect.com/science/article/pii/*
