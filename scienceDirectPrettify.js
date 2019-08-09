640b6d7597065f3907e32770c058ccbe3c6650a4
wolfpan <haoyupan@aliyun.com>
2019-08-09 19:33:12 +0800

    update science direct prettify, fit laptap.

--- scienceDirectPrettify.js
+++ scienceDirectPrettify.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Science Direct Prettify
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Get a better performance when reading papers from science direct website.
 // @author       Haoyu Pan
 // @match        https://www.sciencedirect.com/science/article/pii/*
@@ -13,12 +13,22 @@
     setTimeout(() => {
         document.querySelector('body').innerHTML = document.querySelector('article').innerHTML;
         document.querySelector('body').style.margin = '1rem';
-        document.querySelectorAll('p, h1, h2, h3, h4, h5, h6, li, th, td').forEach(e => {
-            e.style.cssText += `
-font-size: ${Number(window.getComputedStyle(e).fontSize.slice(0, 2)) * 3}px !important;
-line-height: ${Number(window.getComputedStyle(e).fontSize.slice(0, 2)) * 4}px !important;
-`;
-        });
+	if (Number(window.getComputedStyle(document.querySelector('html')).width.slice(0, 4)) < 1300){
+            document.querySelectorAll('p, h1, h2, h3, h4, h5, h6, li, th, td').forEach(e => {
+                e.style.cssText += `
+	            font-size: ${Number(window.getComputedStyle(e).fontSize.slice(0, 2)) * 3}px !important;
+		    line-height: ${Number(window.getComputedStyle(e).fontSize.slice(0, 2)) * 4}px !important;
+		    `;
+            })
+	} else {
+            document.querySelectorAll('p, h1, h2, h3, h4, h5, h6, li, th, td').forEach(e => {
+                e.style.cssText += `
+                    font-size: ${Number(window.getComputedStyle(e).fontSize.slice(0, 2)) * 2}px !important;
+                    line-height: ${Number(window.getComputedStyle(e).fontSize.slice(0, 2)) * 3}px !important;
+                    `;
+            })
+	    
+	}
         document.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach(e => {
             e.style.cssText += `
 color: red;
