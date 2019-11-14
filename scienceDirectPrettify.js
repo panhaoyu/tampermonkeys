450bbef320d21f8e09f196f92f0e625bf246e3ea
wolfpan <haoyupan@aliyun.com>
2019-11-14 14:51:15 +0800

    fix comer bug

--- scienceDirectPrettify.js
+++ scienceDirectPrettify.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Science Direct Prettify
 // @namespace    http://tampermonkey.net/
-// @version      0.4.0
+// @version      0.4.1
 // @description  Get a better performance when reading papers from science direct website.
 // @author       Haoyu Pan
 // @match        https://www.sciencedirect.com/science/article/pii/*
@@ -15,7 +15,7 @@
     function main() {
         document.querySelectorAll("p").forEach(e => {
             e.style.fontFamily = "微软雅黑";
-        })
+        });
 
         window.setTimeout(main,1000);
     }
