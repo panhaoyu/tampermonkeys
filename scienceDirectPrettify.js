fbdede8cfa1087d1ed54c0278b7baf837d95e03b
wolfpan <haoyupan@aliyun.com>
2019-11-14 14:49:59 +0800

    change science direct font family

--- scienceDirectPrettify.js
+++ scienceDirectPrettify.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Science Direct Prettify
 // @namespace    http://tampermonkey.net/
-// @version      0.3.4
+// @version      0.4.0
 // @description  Get a better performance when reading papers from science direct website.
 // @author       Haoyu Pan
 // @match        https://www.sciencedirect.com/science/article/pii/*
@@ -15,8 +15,6 @@
     function main() {
         document.querySelectorAll("p").forEach(e => {
             e.style.fontFamily = "微软雅黑";
-            e.style.fontSize = "20px";
-            console.log(e.style)
         })
 
         window.setTimeout(main,1000);
