12a971b6d3074e7cbe82987a4cfacce94cbf4c11
panhaoyu <panhaoyu.china@outlook.com>
2023-05-18 22:38:45 +0800

    add script: 同济一卡通

---
+++ 同济一卡通.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         同济一卡通处理
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  下载同济一卡通数据
+// @author       Haoyu Pan
+// @match        https://yikatong.tongji.edu.cn/page/page
+// @grant        none
+// ==/UserScript==
+
+(async function () {
+    'use strict';
+
+    alert('hello')
+})();
