222916a36a092e70174fe5064c2365563b7cff43
panhaoyu <panhaoyu.china@outlook.com>
2023-07-13 20:28:57 +0800

    patch

--- tongjiGymBadminton.js
+++ tongjiGymBadminton.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         同济本部羽毛球场馆查询
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://gym.tongji.edu.cn/Pc/
