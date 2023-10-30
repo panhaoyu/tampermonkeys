1f26d9c6b5e4f2e3c7e50204a5c624e1875fd3db
panhaoyu <panhaoyu.china@outlook.com>
2023-10-30 22:41:30 +0800

    patch: chatgpt-browser-api.js, fix selectors

--- chatgpt-browser-api.js
+++ chatgpt-browser-api.js
@@ -1,91 +1,106 @@
 // ==UserScript==
 // @name         ChatGPT API By Browser Script
 // @namespace    http://tampermonkey.net/
-// @version      0.0.2
+// @version      0.0.3
 // @match        https://chat.openai.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=openai.com
 // @grant        none
 // @license MIT
 // ==/UserScript==
 
-const WS_URL = `ws://127.0.0.1:8765`;
+const WS_URL = `ws://127.0.0.1:8765`
 
 function getTextFromNode(node) {
-    let result = '';
+    let result = ''
 
-    if (!node) return result;
-    const childNodes = node.childNodes;
+    if (!node) return result
+    const childNodes = node.childNodes
 
     for (let i = 0; i < childNodes.length; i++) {
-        let childNode = childNodes[i];
+        let childNode = childNodes[i]
         if (childNode.nodeType === Node.TEXT_NODE) {
-            result += childNode.textContent;
+            result += childNode.textContent
         } else if (childNode.nodeType === Node.ELEMENT_NODE) {
-            result += getTextFromNode(childNode);
+            result += getTextFromNode(childNode)
         }
     }
 
-    return result;
+    return result
 }
 
 function sleep(time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
+    return new Promise((resolve) => setTimeout(resolve, time))
 }
+
+function rightArea() {
+    return document.querySelector('[role="presentation"]')
+}
+
+function leftArea() {
+    return document.querySelector('[aria-label="Chat history"]')
+}
+
+function newChatButton() {
+    return Array.from(leftArea().querySelectorAll('a'))
+        .find(i => i.textContent.trim() === 'New Chat')
+}
+
+function gptModel3Button() {
+    return Array.from(rightArea().querySelectorAll('button[type=button]'))
+        .find(i => i.textContent.trim() === 'GPT-3.5')
+}
+
+function gptModel4Button() {
+    return Array.from(rightArea().querySelectorAll('button[type=button]'))
+        .find(i => i.textContent.trim() === 'GPT-4')
+}
+
+function inputArea() {
+    return document.querySelector('textarea')
+}
+
+function sendButton() {
+    return inputArea().nextElementSibling
+}
+
 
 // Main app class
 class App {
     constructor() {
-        this.socket = null;
-        this.observer = null;
-        this.stop = false;
-        this.dom = null;
+        this.socket = null
+        this.observer = null
+        this.stop = false
+        this.dom = null
     }
 
     async start({text, model, newChat}) {
         this.stop = false
         if (newChat) {
-            try {
-                const a = document.querySelector('a');
-                a.click();
-                await sleep(500);
-                const list = [...document.querySelectorAll('li.group.relative.flex')].map(item => ({
-                    text: getTextFromNode(item),
-                    node: item
-                }))
-                if (model === 'gpt-4') {
-                    const find = document.querySelectorAll('.truncate.text-sm.font-medium')[1]
-                    if (find) {
-                        find.click();
-                        await sleep(500);
-                    }
-                } else {
-                    const find = document.querySelectorAll('.truncate.text-sm.font-medium')[0]
-                    if (find) {
-                        find.click();
-                        await sleep(500);
-                    }
-                }
-            } catch (e) {
-                console.log(e);
+            newChatButton().click()
+            await sleep(500)
+            switch (model) {
+                case 'gpt-3':
+                    gptModel3Button().click()
+                    break
+                case 'gpt-4':
+                    gptModel4Button().click()
+                    break
             }
+            await sleep(500)
         }
-        const textarea = document.querySelector('textarea');
-        textarea.value = text;
-        const event = new Event('input', {bubbles: true});
-        textarea.dispatchEvent(event);
-        await sleep(500);
-        const siblingButton = textarea.nextElementSibling;
-        siblingButton.click();
-
-        this.observeMutations();
+        inputArea().value = text
+        inputArea().dispatchEvent(new Event('input', {bubbles: true}))
+        await sleep(500)
+        sendButton().click()
+        this.observeMutations()
     }
 
     observeMutations() {
         this.observer = new MutationObserver(async (mutations) => {
-            const list = [...document.querySelectorAll('div.relative.flex.gap-1')];
-            const last = list[list.length - 1];
-            if (!last) return;
-            const lastText = getTextFromNode(last.querySelector('div.flex.flex-grow.flex-col.gap-3'));
+            const list = [...document.querySelectorAll('[role=presentation] .group [data-message-author-role=assistant]')]
+            const last = list[list.length - 1]
+            if (!last) return
+            const lastText = getTextFromNode(last.querySelector('div.flex.flex-grow.flex-col.gap-3'))
             console.log('send', {
                 text: lastText,
             })
@@ -94,13 +109,11 @@ class App {
                     type: 'answer',
                     text: lastText,
                 })
-            );
-            await sleep(1000);
-            const button = document.querySelector('.btn-neutral');
+            )
+            await sleep(1000)
+            const button = document.querySelector('.btn-neutral')
             if (!button) return
-            if (
-                button.querySelector('div').textContent.trim() != 'Stop generating'
-            ) {
+            if (button.querySelector('div').textContent.trim() !== 'Stop generating') {
                 if (this.stop) return
                 this.stop = true
                 console.log('send', {
@@ -110,63 +123,62 @@ class App {
                     JSON.stringify({
                         type: 'stop',
                     })
-                );
-                this.observer.disconnect();
+                )
+                this.observer.disconnect()
             }
-        });
+        })
 
-        const observerConfig = {childList: true, subtree: true};
-        this.observer.observe(document.body, observerConfig);
+        this.observer.observe(document.body, {childList: true, subtree: true})
     }
 
     sendHeartbeat() {
         if (this.socket.readyState === WebSocket.OPEN) {
-            this.socket.send(JSON.stringify({type: 'heartbeat'}));
+            this.socket.send(JSON.stringify({type: 'heartbeat'}))
         }
     }
 
     connect() {
-        this.socket = new WebSocket(WS_URL);
+        this.socket = new WebSocket(WS_URL)
         this.socket.onopen = () => {
-            console.log('Server connected, can process requests now.');
-            this.dom.innerHTML = '<div style="color: green; ">API Connected !</div>';
+            console.log('Server connected, can process requests now.')
+            this.dom.innerHTML = '<div style="color: green ">API Connected !</div>'
         }
         this.socket.onclose = () => {
-            console.log('The server connection has been disconnected, the request cannot be processed.');
-            this.dom.innerHTML = '<div style="color: red; ">API Disconnected !</div>';
+            console.log('The server connection has been disconnected, the request cannot be processed.')
+            this.dom.innerHTML = '<div style="color: red ">API Disconnected !</div>'
 
             setTimeout(() => {
-                console.log('Attempting to reconnect...');
-                this.connect();
-            }, 2000);
+                console.log('Attempting to reconnect...')
+                this.connect()
+            }, 2000)
         }
         this.socket.onerror = () => {
-            console.log('Server connection error, please check the server.');
-            this.dom.innerHTML = '<div style="color: red; ">API Error !</div>';
+            console.log('Server connection error, please check the server.')
+            this.dom.innerHTML = '<div style="color: red ">API Error !</div>'
         }
         this.socket.onmessage = (event) => {
             const data = JSON.parse(event.data)
             console.log('params', data)
-            this.start(data);
-        };
+            this.start(data)
+        }
     }
 
     init() {
         window.addEventListener('load', () => {
-            this.dom = document.createElement('div');
-            this.dom.style = 'position: fixed; top: 10px; right: 10px; z-index: 9999; display: flex; justify-content: center; align-items: center;';
-            document.body.appendChild(this.dom);
+            this.dom = document.createElement('div')
+            this.dom.style = 'position: fixed top: 10px right: 10px z-index: 9999 display: flex justify-content: center align-items: center'
+            document.body.appendChild(this.dom)
 
-            this.connect();
+            this.connect()
 
-            setInterval(() => this.sendHeartbeat(), 30000);
-        });
+            setInterval(() => this.sendHeartbeat(), 30000)
+        })
     }
 
 }
 
 (function () {
-    'use strict';
-    const app = new App();
-    app.init();
-})();
+    'use strict'
+    const app = new App()
+    app.init()
+})()
